7e2a0f2b3731d3b4eb70084b47ef8a8382a4ac2f
Artur Koczaja <artur.koczaja@sdworx.com>
2022-05-25 15:08:14 +0200

    clean

--- XlsxMarge/Program.cs
+++ XlsxMarge/Program.cs
@@ -11,20 +11,20 @@ using System.Runtime.CompilerServices;
 using System.Xml;
 using Common.Logging;
 using Common.Logging.Factory;
+using Microsoft.Office.Interop.Excel;
 
 namespace XlsxMarge
 {
     internal class Program
     {
-
-
+        
         static void Main(string[] args)
         {
-           
+
             string[] inputFiles = new string[]
             {
-                @"C:\tfs\trening\excelFile\1RX3015_20220512_Testberekening - XLSX_1.xlsx",
-                @"C:\tfs\trening\excelFile\1RX3015_20220512_Testberekening - XLSX_2.xlsx"
+                @"C:\tfs\trening\excelFile\1RX3015_20220512_Testberekening-XLSX_1.xlsx",
+                @"C:\tfs\trening\excelFile\1RX3015_20220512_Testberekening-XLSX_2.xlsx"
             };
 
             string[] outputFiles = new string[]
@@ -32,9 +32,9 @@ namespace XlsxMarge
                 @"C:\tfs\trening\excelFile\output.xlsx"
             };
 
-            ZipDirectory(@"C:\tfs\trening\excelFile\1RX3015_20220512_Testberekening - XLSX_result.xlsx", "output.xlsx");
-            UnzipFile("1RX3015_20220512_Testberekening - XLSX_1.xlsx", "TemporaryDirectory");
             
         }
+
+    }
 }
 
